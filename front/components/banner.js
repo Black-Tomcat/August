bba9cb1b0dc73ba156722b6916d8db467e997d2a
silentcoder99 <silentcoder99@gmail.com>
2018-08-12 01:38:31 +1000

    Actually add less cramped logo

--- front/components/banner.js
+++ front/components/banner.js
@@ -6,7 +6,7 @@ export default class Banner extends Component {
     render() {
         return (
             <div>
-                <Image src='https://imgur.com/a/dBnqOCP.png' size='medium' centered/>
+                <Image src='https://i.imgur.com/ziTsCmj.png' size='medium' centered/>
             </div>
         )
     }
