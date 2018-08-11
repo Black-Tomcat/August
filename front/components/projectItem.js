e030acf244dc886756edc851fbee86e65ae7aae5
MatthewDOTexecutable <mattiuspotts@gmail.com>
2018-08-11 20:59:11 +1000

    Created project card component

---
+++ front/components/projectItem.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Item, Image, Loader} from 'semantic-ui-react';
+import Client form './client';
+
+
+export default class ProjectItem extends Component{
+    contructor(props){
+        super(props);
+
+        this.setState = {
+            loaded: false;
+        }
+    }
+
+    componentDidMount(){
+        let client = new Client();
+        client.getProject(this.props.projectId, (projectInfo) => {
+            projectInfo.loaded = true;
+            this.setState(projectInfo);
+        });
+    }
+
+    render(){
+        return 
+            <Item>
+                <Loader active={!this.state.loaded} />
+                {
+                loaded && <Item.Image src={this.state.img} />
+                <Item.Content>
+                    <Item.Header>{this.state.name}</Item.Header>
+                    <Item.Description>{this.state.description.substring(0, 30)}</Item.description
+                </Item.Content>
+            </Item>
+    }
+}
