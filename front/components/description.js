982c994970050cc7bec49aeffb7f1a77175aa884
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-11 14:58:05 +1000

    Typo

--- front/components/description.js
+++ front/components/description.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 
 export default class Description extends Component {
     render() {
+        const {description} = this.props;
         return (
             <div>
 
