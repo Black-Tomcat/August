e5b8d4926c183e4e4d9ad1a4098ec6e055847fa7
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-11 13:51:04 +1000

    Template project.

---
+++ front/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import ReactDOM from 'react-dom';
+
+
+class Index extends Component {
+    render() {
+        return (<p>HEY THERE</p>)
+    }
+}
+
+ReactDOM.render(
+    <Index/>,
+    document.body
+);
