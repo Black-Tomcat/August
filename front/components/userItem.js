d380810f71bf45aef0ebef718e238d90347a7321
Matt Potts <mattiuspotts@gmail.com>
2018-08-12 03:02:10 +1000

    Added bio and Skills to userItem component

--- front/components/userItem.js
+++ front/components/userItem.js
@@ -1,28 +1,29 @@
-import React, {Component} from 'react';
-import {Item, Image, Loader} from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Item, Image, Loader } from "semantic-ui-react";
 
+export default class UserItem extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class UserItem extends Component{
-    constructor(props){
-        super(props);
+  changePage = () => {
+    this.props.transitionPage("user", this.props.user._id);
+  };
 
-    }
-
-    changePage = () => {
-        this.props.transitionPage("user", this.props.user._id)
-    };
-
-    render(){
-        const {name, degree, profile} = this.props.user;
-
-        return (
-            <Item className="objectItems">
-                <Item.Image src={profile}/>
-                <Item.Content>
-                    <Item.Header as="a" onClick={this.changePage}>{name}</Item.Header>
-                    <Item.Description>{degree}</Item.Description>
-                </Item.Content>
-            </Item>
-        );
-    }
-};
+  render() {
+    const { name, degree, profile, bio, skills } = this.props.user;
+    return (
+      <Item className="objectItems">
+        <Item.Image src={profile} />
+        <Item.Content>
+          <Item.Header as="a" onClick={this.changePage}>
+            {name}
+          </Item.Header>
+          <Item.Meta>{degree}</Item.Meta>
+          <Item.Description>{bio.substring(0, 30)}</Item.Description>
+          <Item.Extra >{"Skills: " + skills.join(", ")}</Item.Extra>
+        </Item.Content>
+      </Item>
+    );
+  }
+}
