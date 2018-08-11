da2b51d33ad348ee811b2c7aff23491bf94b7b04
silentcoder99 <silentcoder99@gmail.com>
2018-08-11 14:55:41 +1000

    Add basic Header component

--- front/containers/userPage.js
+++ front/containers/userPage.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import Header from '../components/header.js'
 
-
-export default class userPage extends Componet {
+export default class UserPage extends Component {
     render() {
+        let headingTags = ["Bachelor of Compute", "Looking for a mentor"];
 
+        let hTagComponents = headingTags.map((tagText) => {
+            return (
+                <p>{tagText}</p>
+            )
+        });
+
+        return (
+            <div>
+                <Header
+                    heading="Name"
+                    headingTags={hTagComponents}
+                />
+            </div>
+        )
     }
 }
+
