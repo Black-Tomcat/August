425bee4f2eec1915741ba82232184ce592409a2d
Sam Nolan <samnolan555@gmail.com>
2018-08-11 20:23:03 +1000

    Add UserItem

---
+++ front/components/userItem.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Item, Image, Loader} from 'semantic-ui-react';
+import Client from '../client'
+
+
+export default class UserItem extends Component{
+    constructor(props){
+        super(props);
+        
+        this.setState = {
+            loaded: false;
+        }
+    }
+
+
+    componentDidMount(){
+        let client = new Client();
+        client.getUser(this.props.userId, (userInfo) => {
+            userInfo.loaded = true;
+            this.setState(userInfo);
+        });
+    }
+
+    render(){
+        return (
+            <Item>
+                <Loader active={!this.state.loaded} />
+                {
+                loaded && <Item.Image src={this.state.profile} />
+                <Item.Content>
+                    <Item.Header>{this.state.name}</Item.Header>
+                    <Item.Description>{this.state.degree}</Item.Description>
+                </Item.Content> 
+                }
+            </Item>
+        );
+    }
+};
