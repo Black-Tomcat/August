1430a640e67f9db7d8dbdafca15765c343b91868
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-11 15:07:43 +1000

    Imports

--- front/containers/projectPage.js
+++ front/containers/projectPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import Description from '../components/description';
+
 
 export default class ProjectPage extends Component {
     constructor(props) {
--- front/index.js
+++ front/index.js
@@ -14,6 +14,7 @@ class Index extends Component {
             loggedIn: false,
             viewing: "user",
             pageInfo: "Sam",
+
             //
             users: {}
         }
