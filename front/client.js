48bb04cf7b92ebef6b0a1d46325f8917c967f2ea
Sam Nolan <samnolan555@gmail.com>
2018-08-11 16:32:25 +1000

    Move client to point to Sam's PC

--- front/client.js
+++ front/client.js
@@ -1,25 +1,26 @@
 
+const url = "10.132.46.98";
 export default class BackendClient{
     addUser(user, callback) {
-        fetch('http://localhost:3000/users',{method: 'POST', body: JSON.stringify(user)}).then((res) => {
+        fetch('http://' + url + ':3000/users',{method: 'POST', body: JSON.stringify(user)}).then((res) => {
             callback(res.status, res.text());
         });
     }
 
     addProject(project, callback) {
-        fetch('http://localhost:3000/projects', {method: 'POST', body: JSON.stringify(project)}).then((res) => {
+        fetch('http://' + url + ':3000/projects', {method: 'POST', body: JSON.stringify(project)}).then((res) => {
             callback(res.status, res.text());
         });
     }
 
     getUser(id, callback) {
-        fetch('http://localhost:3000/users/' + id).then((res) => {
+        fetch('http://' + url + ':3000/users/' + id).then((res) => {
             callback(res.status, res.json());
         });
     }
 
     getProject(id, callback) {
-        fetch('http://localhost:3000/projects/' + id).then((res) => {
+        fetch('http://' +url + ':3000/projects/' + id).then((res) => {
             callback(res.status, res.json());
         });
     }
