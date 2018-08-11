e1a02d1237477376a7e26b5a8840e6511905c670
Sam Nolan <samnolan555@gmail.com>
2018-08-11 18:03:33 +1000

    Add registering page

--- front/containers/loginPage.js
+++ front/containers/loginPage.js
@@ -1,29 +1,46 @@
 import React, {Component} from 'react';
 import {Input, Button, Grid} from 'semantic-ui-react';
+import NewUserPage from './newUserPage'
 
 
 export default class LoginPage extends Component {
     constructor(props){
         super(props);
         this.username = "";
+        this.state = {
+            registering: false
+        }
     }
 
+    registerUser = () => {
+        this.setState({registering: true});
+    }
+
+    newUser = () => {
+        this.setState({registering: false});
+    }
 
     render() {
-        return (
-            <div className="passwordForm">
-                <Grid>
-                <Grid.Row centered columns={1}>
-                    <Grid.Column width={3}>
-                        <Input type='text' placeholder="Username" onChange={this.usernameChange} />
-                        <br />
-                        <Input type='password' placeholder="Password" />
-                        <Button type='submit' onClick={this.submitted}>Submit</Button>
-                    </Grid.Column>
-                </Grid.Row>
-                </Grid>
-            </div>
-        );
+        if (this.state.registering){
+            return <NewUserPage onNewUser={this.newUser}/>
+        }
+        else{
+            return (
+                <div className="passwordForm">
+                    <Grid>
+                    <Grid.Row centered columns={1}>
+                        <Grid.Column width={3}>
+                            <Input type='text' placeholder="Username" onChange={this.usernameChange} />
+                            <br />
+                            <Input type='password' placeholder="Password" />
+                            <Button type='submit' onClick={this.submitted}>Submit</Button>
+                            <Button onClick={this.registerUser}>New User? Register!</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                    </Grid>
+                </div>
+            );
+        }
     }
 
     usernameChange = (event, data) => {
---
+++ front/containers/newUserPage.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Header, Form} from 'semantic-ui-react';
+import Client from '../client.js';
+
+export default class NewUserPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    handleChange = (e, {name, value}) => {
+        this.setState({[name]: value});
+    }
+
+    onSubmit = () => {
+        let client = new Client();
+        client.addUser({
+            name: this.state.username,
+            degree: this.state.degree,
+            skills: this.state.skills.split("\n"),
+            bio: this.state.bio,
+            profile: this.state.profile
+        }, (status, id) => {
+            this.props.onNewUser(id);
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <Header>Create new user</Header>
+            <p> Welcome to August! Please fill in the following form to create
+                a new user </p>
+            <Form onSubmit={this.onSubmit}>
+                <Form.Input label="Username" name="username" onChange={this.handleChange} />
+                <Form.Input type="password" label="Password" name="password" onChange={this.handleChange} />
+                <Form.Input label="Degree" name="degree" onChange={this.handleChange} />
+                <Form.Input label="Profile" name="profile" onChange={this.handleChange} />
+                <Form.Input label="Looking For" name="looking" onChange={this.handleChange} />
+                <Form.TextArea label="bio" placeholder="Tell us a little bit about yourself" name="bio" onChange={this.handleChange} />
+                <Form.TextArea label="skills" name="skills" onChange={this.handleChange} />
+                <Form.Button content="Submit" type="submit"/>
+            </Form>
+        </div>
+        );
+    }
+};
