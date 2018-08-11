e6b2a60de89e459fc30fcde3c0401926ffe972a2
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-11 14:29:48 +1000

    Updated with template.

--- front/containers/userPage.js
+++ front/containers/userPage.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 
 export default class userPage extends Componet {
-    render() {
+    static PropTypes = {
+        user: PropTypes.object.required
+    };
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        const {user} = this.props;
+        const {} = this.state;
+
+        return (
+            <div>
+
+            </div>
+        );
     }
 }
