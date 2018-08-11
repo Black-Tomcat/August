1ff0ffcaa76ff224c4bfc0c834621eda855af5c5
Sam Nolan <samnolan555@gmail.com>
2018-08-11 18:02:58 +1000

    Fix up client
    With JSON Headers a

--- front/client.js
+++ front/client.js
@@ -2,26 +2,37 @@
 const url = "10.132.46.98";
 export default class BackendClient{
     addUser(user, callback) {
-        fetch('http://' + url + ':3000/users',{method: 'POST', body: JSON.stringify(user)}).then((res) => {
-            callback(res.status, res.text());
+        fetch('http://' + url + ':3000/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)}).then((res) => res.text()).then((res) => {
+            callback(res.status, res);
         });
     }
 
     addProject(project, callback) {
-        fetch('http://' + url + ':3000/projects', {method: 'POST', body: JSON.stringify(project)}).then((res) => {
-            callback(res.status, res.text());
+        fetch('http://' + url + ':3000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        }).then((res) => res.text()).then((res) => {
+            callback(res.status, res);
         });
     }
 
     getUser(id, callback) {
-        fetch('http://' + url + ':3000/users/' + id).then((res) => {
-            callback(res.status, res.json());
+        fetch('http://' + url + ':3000/users/' + id).then((res) => res.json()).then((res) => {
+            callback(res.status, res);
         });
     }
 
     getProject(id, callback) {
-        fetch('http://' +url + ':3000/projects/' + id).then((res) => {
-            callback(res.status, res.json());
+        fetch('http://' +url + ':3000/projects/' + id).then((res) => res.json()).then((res) => {
+            callback(res.status, res);
         });
     }
 }
